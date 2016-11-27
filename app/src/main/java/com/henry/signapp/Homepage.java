cf1d635a3141812f7cb68d7b084bdb7abc4c07b5
tom <tom@debian>
2016-11-26 20:42:01 -0500

    login complete

--- app/src/main/java/com/henry/signapp/Homepage.java
+++ app/src/main/java/com/henry/signapp/Homepage.java
@@ -4,16 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Homepage extends AppCompatActivity {
+    private FirebaseAuth auth;
+
 
     //list arrays for testing
     private String[] decks = {"Alphabet & Numbers", "Shapes", "Colors"};
@@ -30,21 +32,22 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         Firebase.setAndroidContext(this);
-        final ListView deckListView = (ListView) findViewById(R.id.decks);
 
-        firebaseButton = (Button) findViewById(R.id.firebase);
-        mRef = new Firebase("https://signapp-aab9e.firebaseio.com/");
-        mGifsRef = new Firebase("https://signapp-aab9e.firebaseio.com/gifs");
-        ArrayAdapter deckAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item,
-                android.R.id.text1, decksList);
-        deckListView.setAdapter(deckAdapter);
+        auth = FirebaseAuth.getInstance();
+        String user = auth.getCurrentUser().getEmail();
+        TextView welcome = (TextView) findViewById(R.id.welcomeText);
+        welcome.setText("Welcome " + user);
 
-    }
 
-    public void firebase(View v){
-       // Create Child and set its value
-//        Firebase mRefChild = mRef.child("Name");
-//        mRefChild.setValue("HenryTest");
+//        final ListView deckListView = (ListView) findViewById(R.id.decks);
+//
+//        firebaseButton = (Button) findViewById(R.id.firebase);
+//        mRef = new Firebase("https://signapp-aab9e.firebaseio.com/");
+//        mGifsRef = new Firebase("https://signapp-aab9e.firebaseio.com/gifs");
+//        ArrayAdapter deckAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item,
+//                android.R.id.text1, decksList);
+//        deckListView.setAdapter(deckAdapter);
+
     }
 
     //practice button listener
@@ -52,6 +55,12 @@ public class Homepage extends AppCompatActivity {
         Intent practiceIntent = new Intent(this, Practice.class);
 
         startActivity(practiceIntent);
+    }
+
+    //logout button listener
+    public void logout(View view){
+        auth.signOut();
+        finish();
     }
 
 }
